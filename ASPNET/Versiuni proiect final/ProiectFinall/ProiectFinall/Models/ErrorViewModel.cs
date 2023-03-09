f213c5ee911a8364e7f266d41072a277bcb65c4d
StefanATudose <95222954+StefanATudose@users.noreply.github.com>
2023-03-09 20:21:24 +0200

    Deleted old versions

--- ASPNET/Versiuni proiect final/ProiectFinall/ProiectFinall/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ProiectFinall.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
